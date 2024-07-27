db33127a5bac8bd406eb2e63c207dc631dd9193b
unknown <sc2tvsibiya@gmail.com>
2024-07-27 11:10:05 +0300

    add cora

---
+++ apps/cora/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, NxWelcomeComponent],
+  selector: 'si-cora-cora-entry',
+  template: `<si-cora-nx-welcome></si-cora-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
